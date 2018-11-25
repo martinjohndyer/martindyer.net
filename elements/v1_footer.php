8236a75ff00c953e40a70bcc2a732da6f14d93d5
Martin Dyer <martinjohndyer@gmail.com>
2018-11-25 16:33:13 +0000

    Remove old files

--- elements/v1_footer.php
+++
@@ -1,5 +0,0 @@
-	<!-- Footer -->
-	<div class="mjd-grey w3-container w3-padding-32" id="footer" style="margin-top:75px;padding-right:58px">
-		<p class="w3-right">Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a></p>
-		<p class="w3-left">Copyright &copy; <a href="/" title="Martin J Dyer" target="_blank" class="w3-hover-opacity">Martin J Dyer</a>. All rights reserved. </p>
-	</div>
